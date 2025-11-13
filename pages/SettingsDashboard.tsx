a6ea1262112a04a66ee479e31035c9e5f700b89b
samnangh849-source <samnangh849@gmail.com>
2025-11-13 11:42:03 +0700

    Test5

--- pages/SettingsDashboard.tsx
+++ pages/SettingsDashboard.tsx
@@ -3,8 +3,8 @@ import React, { useState, useContext, useEffect, useRef } from 'react';
 import { AppContext } from '../App';
 import Spinner from '../components/common/Spinner';
 import Modal from '../components/common/Modal';
-import { WEB_APP_URL } from '../../constants';
-import { fileToBase64, convertGoogleDriveUrl } from '../../utils/fileUtils';
+import { WEB_APP_URL } from '../constants';
+import { fileToBase64, convertGoogleDriveUrl } from '../utils/fileUtils';
 
 interface SettingsDashboardProps {
     onBack: () => void;
@@ -186,7 +186,7 @@ const ConfigEditModal = ({ section, item, onClose, onSave }: { section: typeof c
                                     {passwordVisibility[field.name] ? (
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7 1.274-4.057 5.064-7 9.542-7 .847 0 1.67 .126 2.454 .364m-3.033 2.446a3 3 0 11-4.243 4.243m4.242-4.242l4.243 4.243M3 3l18 18" /></svg>
                                     ) : (
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268-2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg>
                                     )}
                                 </button>
                             </div>
