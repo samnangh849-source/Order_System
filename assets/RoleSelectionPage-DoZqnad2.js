2e3b29673616cc342246a00d2202243e7b5b28ec
samnangh849-source <samnangh849@gmail.com>
2025-12-02 02:22:44 +0700

    Updates

--- assets/RoleSelectionPage-DoZqnad2.js
+++ assets/RoleSelectionPage-DoZqnad2.js
@@ -1,4 +1,4 @@
-import{j as e}from"./index-5_csBKuw.js";const s=({onSelect:r})=>e.jsxs("div",{className:"min-h-screen flex flex-col items-center justify-center p-4 animate-fade-in bg-gradient-to-br from-gray-900 to-gray-800",children:[e.jsx("style",{children:`
+import{j as e}from"./index-BGJhT3oz.js";const s=({onSelect:r})=>e.jsxs("div",{className:"min-h-screen flex flex-col items-center justify-center p-4 animate-fade-in bg-gradient-to-br from-gray-900 to-gray-800",children:[e.jsx("style",{children:`
                 @keyframes fade-in {
                     from { opacity: 0; transform: translateY(20px); }
                     to { opacity: 1; transform: translateY(0); }
